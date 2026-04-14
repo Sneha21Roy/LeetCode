05d2db303e9900cad9e634f8f133b5f9b7dc971b
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-04-14 13:16:29 +0530

    Time: 1 ms (96.54%), Space: 42.4 MB (89.44%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // Base cases
+        if (n == 1) return true;
+        if (n <= 0 || n % 2 != 0) return false;
+
+        // Recursive step
+        return isPowerOfTwo(n / 2);
+    }
+}
