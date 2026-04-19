cdc1afcfa7a070f2ed8e13ec04e02066ecc7a876
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-04-19 21:25:55 +0530

    Time: 5 ms (5.2%), Space: 46.4 MB (77.27%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,49 +1,34 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-       
-        if(root==null){
+        if (root == null) {
             return root;
         }
-        Queue<TreeNode> queue=new LinkedList<>();
+
+        Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
-        while(!queue.isEmpty()){
-            int size=queue.size();
-        List<TreeNode> level=new ArrayList<>();
-        for(int i=0;i<size;i++){
-            TreeNode current=queue.poll();
-            if(current.left!=null){
-                queue.offer(current.left);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Node> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+                level.add(current);
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
             }
-             if(current.right!=null){
-                queue.offer(current.right);
+
+            for (int j = 0; j < level.size() - 1; j++) {
+                level.get(j).next = level.get(j + 1);
             }
-            level.add(current);
+        }
+
+        return root;
     }
-    for(j=0;j<i.size()-1;j++){
-        level.get(i).next=i.get(i+1);
-    }
-    return root;
 }
