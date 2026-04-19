b6f9b84b703f790effba4bab13e5a1445ec04a6a
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-04-19 21:21:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,48 +1,33 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        if (root == null) return null;
+        if (root == null) {
+            return root;
+        }
 
-        Node leftmost = root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
 
-        while (leftmost.left != null) {
-            Node head = leftmost;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node prev = null;  // 👈 track previous node
 
-            while (head != null) {
-                // connect left -> right
-                head.left.next = head.right;
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
 
-                // connect right -> next left
-                if (head.next != null) {
-                    head.right.next = head.next.left;
+                // 🔥 connect previous node to current
+                if (prev != null) {
+                    prev.next = current;
+                }
+                prev = current;
+
+                if (current.left != null) {
+                    queue.offer(current.left);
                 }
 
-                head = head.next;
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
             }
-
-            leftmost = leftmost.left;
         }
 
         return root;
