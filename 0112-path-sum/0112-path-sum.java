ab4ecff72120291b1a28c8fc6850ad84f6e91a53
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-04-19 22:00:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,15 +15,34 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null) return false;
+        if (root == null) return false;
 
-        
-        if(root.left == null && root.right == null){
-            return targetSum == root.val;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> sumStack = new Stack<>();
+
+        nodeStack.push(root);
+        sumStack.push(root.val);
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            int sum = sumStack.pop();
+
+            // check leaf
+            if (node.left == null && node.right == null) {
+                if (sum == targetSum) return true;
+            }
+
+            if (node.right != null) {
+                nodeStack.push(node.right);
+                sumStack.push(sum + node.right.val);
+            }
+
+            if (node.left != null) {
+                nodeStack.push(node.left);
+                sumStack.push(sum + node.left.val);
+            }
         }
 
-        
-        return hasPathSum(root.left, targetSum - root.val) ||
-               hasPathSum(root.right, targetSum - root.val);
+        return false;
     }
 }
