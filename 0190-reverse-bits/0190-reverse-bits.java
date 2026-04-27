d876b19296afc1bd1502b7de413286b675488df5
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-04-27 22:55:19 +0530

    Time: 1 ms (60.56%), Space: 41.9 MB (98.03%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            result = result << 1;      // left shift result
+            result = result | (n & 1); // add last bit of n
+            n = n >> 1;               // shift n
+        }
+        
+        return result;
+    }
+}
