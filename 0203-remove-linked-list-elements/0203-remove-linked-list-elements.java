d188e8453fa5109f61cf1f08036a054e89b5640d
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-07-28 15:36:11 +0530

    Time: 1 ms (97.18%), Space: 47.2 MB (59.98%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,15 +10,23 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+       // val = jis value wali nodes ko linked list se remove karna hai
+        // Agar starting mein val hai,
+        // toh head ko aage move karo
         while(head!=null && head.val==val){
             head=head.next;
         }
+        // Current ko head se start karo
         ListNode current=head;
-        while(current!=null && current.next!=null){
+        // Jab tak next node exist karti hai
+         while(current!=null && current.next!=null){
+             // Kya next node ki value delete wali value hai?
             if(current.next.val==val){
+               //true--> next node ko skip kar do
                 current.next=current.next.next;
             }
             else{
+
                 current=current.next;
             }
         }
