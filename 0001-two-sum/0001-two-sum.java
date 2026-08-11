c1a4edbe8225d1ed79acdaf8bc90258b363c9ebe
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-08-11 23:13:13 +0530

    Time: 2 ms (99.34%), Space: 47.3 MB (23.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,46 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         // for(int i=0;i<nums.length;i++){
+//         //     for(int j=i+1;j<nums.length;j++){
+//         //         if(nums[i]+nums[j]==target){
+//         //              return new int[]{i, j};
+//         //         }
+//         //     }
+//         // }
+//         // return new int[]{};
+//         int left=0;
+//         int right=nums.length-1;
+//         while(left<right){
+//             int sum=nums[left]+nums[right];
+//             if(sum==target){
+//                 return new int[]{left,right};
+//             }
+//             else if(sum<target){
+//                 left++;
+//             }
+//             else{
+//                 right--;
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //BRUTE FORCE APPROACH TC: O(N2) SC=O(1)
-    //     for(int i=0;i<nums.length;i++){
-    //        for(int j=i+1;j<nums.length;j++){
-    //         if(nums[i]+nums[j]==target){
-    //            return new int[]{i, j};
-    //         }
-    //        }
-    //     }
-    //     return new int[]{};
-    // }
-    HashMap<Integer,Integer> map=new HashMap<>();
-    for(int i=0;i<nums.length;i++){
-        int complement=target-nums[i];
-    if(map.containsKey(complement)){
-        return new int[]{map.get(complement),i};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
     }
-    map.put(nums[i],i);
-    }
-    return new int[]{};
-}
 }
