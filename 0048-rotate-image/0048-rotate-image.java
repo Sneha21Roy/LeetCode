04a75a6eaf984e15e9dd86340d28413d573e8cdc
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-08-13 15:45:39 +0530

    Time: 0 ms (100%), Space: 43.5 MB (94.34%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,7 +3,7 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        // Step 1: Transpose
+        // Step 1: Transpose means row ko column bnana or column ko row
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 int temp = matrix[i][j];
