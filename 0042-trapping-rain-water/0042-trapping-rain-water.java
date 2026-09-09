80a61629effd9409211a374a36296efc5205e84e
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-09-09 21:30:30 +0530

    Time: 1 ms (56.68%), Space: 48.7 MB (21.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -10,13 +10,13 @@ class Solution {
         int[] leftMax = new int[n];
         int[] rightMax = new int[n];
 
-        // Build leftMax array
+        //leftMax array
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
 
-        // Build rightMax array
+        // rightMax array
         rightMax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             rightMax[i] = Math.max(rightMax[i + 1], height[i]);
