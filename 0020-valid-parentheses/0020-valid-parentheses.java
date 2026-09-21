3618e6af8d43a362bd3d57672939643ca6ead87f
Sneha Kumari <130816127+Sneha21Roy@users.noreply.github.com>
2026-09-21 22:22:04 +0530

    Time: 3 ms (86.03%), Space: 43.5 MB (25.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,37 @@
-import java.util.*;
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='('||ch=='{'||ch=='['){
-                st.push(ch);// when the character is opening bracket it will be pushed
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            // Opening bracket
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
             }
-            else{
-                if(st.isEmpty()) return false;// If stack is empty → no opening bracket to match
-                char top=st.pop();
-                if((ch==')'&& top!='(')|| //when ever the bracket is closed it will give false
-                  (ch=='}' && top!='{')||
-                  (ch==']' && top!='['))
-                  return false;
+
+            // Closing bracket
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+
+                    return false;
+                }
             }
         }
-        return st.isEmpty();
+
+        return stack.isEmpty();
     }
-    
 }
